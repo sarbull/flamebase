5f1cf3262eab8a08a54430f5c657ed7e5152d9cb
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2018-08-31 23:53:22 +0300

    Move to express

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-const http = require('http');
-const url = require('url');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = http.createServer ((request, response) => {
-  response.writeHead(200, {'Content-type':'application/json'});
-  response.write(JSON.stringify({message: 'Hello world!'}));
-  response.end();
+const app = express();
 
-  pathName = url.parse(request.url).pathname;
-  query = url.parse(request.url).query;
+app.set('etag', false);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+  res.removeHeader('X-Powered-By');
+  res.removeHeader('ETag');
+  next();
+});
 
-  console.log(`pathName = ${pathName}`);
-  console.log(`query = ${query}`);
-})
+app.use(function (req, res) {
+  const url = req.url;
+  const body = req.body;
+  const query = req.query;
+  const headers = req.headers;
+  const method = req.method;
+
+  console.log(`url = ${JSON.stringify(url)}`);
+  console.log(`body = ${JSON.stringify(body)}`);
+  console.log(`query = ${JSON.stringify(query)}`);
+  console.log(`headers = ${JSON.stringify(headers)}`);
+  console.log(`method = ${JSON.stringify(method)}`);
+
+
+  res.json({message: 'Hello world!'});
+});
 
 app.listen(8080);
